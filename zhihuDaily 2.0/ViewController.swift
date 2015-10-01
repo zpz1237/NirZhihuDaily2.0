d918104cf5b37e3feaf825f668ce3a39d190a0b9
NSNirvana <NSNirvana@iCloud.com>
2015-10-01 10:06:14 +0800

    Initial Commit

---
+++ zhihuDaily 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zhihuDaily 2.0
+//
+//  Created by Nirvana on 10/1/15.
+//  Copyright © 2015 NSNirvana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
