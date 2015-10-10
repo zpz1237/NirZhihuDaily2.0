333a74f9e8bd69f2eb4f402bdbaaba5250721c0f
NSNirvana <NSNirvana@iCloud.com>
2015-10-10 18:24:58 +0800

    首页过往文章数据结构设计 以及自定义SectionHeaderUI基础效果完成

---
+++ zhihuDaily 2.0/TableSeparatorViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableSeparatorViewCell.swift
+//  zhihuDaily 2.0
+//
+//  Created by Nirvana on 10/10/15.
+//  Copyright © 2015 NSNirvana. All rights reserved.
+//
+
+import UIKit
+
+class TableSeparatorViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
